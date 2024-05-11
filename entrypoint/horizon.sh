7eb8d6e516449a97075d16d2e9a0de0ed4ae6f44
Jay Rogers <jay@521dimensions.com>
2024-05-10 20:12:25 -0500

    Improved script UX

--- entrypoint/horizon.sh
+++ entrypoint/horizon.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 stop_horizon() {
-  echo "⏬ Shutting down horizon..."
+  echo "⏬ Received signal: $1, shutting down Horizon..."
   php /var/www/html/artisan horizon:terminate
   sleep 5
+  echo "🛑 Horizon service in container '$HOSTNAME' has been shutdown."
   exit 0
 }
 
-trap stop_horizon TERM
+# Catch signals: INT (Ctrl+C), TERM (kill), HUP (hangup), and QUIT
+trap 'stop_horizon INT' INT
+trap 'stop_horizon TERM' TERM
+trap 'stop_horizon HUP' HUP
+trap 'stop_horizon QUIT' QUIT
 
-echo "🚀 Starting horizon..."
+echo "🚀 Starting Horizon container '$HOSTNAME'..."
 php /var/www/html/artisan horizon &
 PID=$!
 wait $PID
